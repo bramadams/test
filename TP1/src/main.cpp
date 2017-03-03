894f9052aee1f666767680240a27fc82515b0c97
bramadams <yasser.araf@belgacom.net>
2017-03-02 22:28:46 -0500

    Using counter on words read in main.

--- TP1/src/main.cpp
+++ TP1/src/main.cpp
@@ -13,6 +13,7 @@ using namespace std;
 
 /* This function reads a file, then prints it, then tries out HashMap functions. */
 int main(int argc, char** argv) {
+  HashMap map;
 
   if(argc<=1){
     cout << "Need file name as argument!" << endl;
@@ -22,30 +23,9 @@ int main(int argc, char** argv) {
     file.open(argv[1]);
     string word;
     while (file >> word){
+      map.compteur(word);
       cout << word << endl;
     }
-  }
-
-  //utilisation normale
-  HashMap hmap;
-  hmap.put("v1",1);
-  hmap.put("v3",1);
-  hmap.put("v11",1);
-  hmap.put("v13",13);
-  
-  int value;
-  bool res = hmap.get("v13", value);
-  if (res)
-    cout << value << endl;
-  
-  hmap.remove("v1");
-  res = hmap.get("v1", value);
-  if (res)
-    cout << "ERROR: " << value << endl;
-
-  std::vector<std::string> keys=hmap.getKeys();
-  for(int i=0;i<keys.size();i++){
-    cout << keys[i] << endl;
   }
   
   return 0;
